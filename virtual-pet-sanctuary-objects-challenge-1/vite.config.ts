a28412cb4d7fa2777780f8acc44548064cbefd71
Mir Ashiful Karim <MirAshiful.Karim@triosstudent.com>
2025-06-19 17:56:33 -0600

    remove unwanted fils 08

--- virtual-pet-sanctuary-objects-challenge-1/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    include: ['tests/**/*.test.js'],
-    setupFiles: './tests/setup.js',
-  },
-});
