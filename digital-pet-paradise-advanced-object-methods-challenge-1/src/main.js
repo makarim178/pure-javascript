5283def133efb19fbdb0e3970b2b846c29c44704
Mir Ashiful Karim <MirAshiful.Karim@triosstudent.com>
2025-06-20 18:28:41 -0600

    digital pet paradise challenge complete

--- digital-pet-paradise-advanced-object-methods-challenge-1/src/main.js
+++ digital-pet-paradise-advanced-object-methods-challenge-1/src/main.js
@@ -11,6 +11,7 @@ import {
 
 // 1. Pet Attribute List
 export function listPetAttributes (pet) {
+  return Object.keys(pet)
 }
 
 
@@ -18,7 +19,8 @@ renderPetPetAttributes(listPetAttributes(pikachu))
 
 // 2. Pet Health Index Calculation
 export function calculatePetHealth (petStatus) {
-
+  const statusAttrs = Object.keys(petStatus)
+  return statusAttrs.reduce((accHealth, key) => accHealth + petStatus[key], 0) / statusAttrs.length
 }
 
 
@@ -27,6 +29,10 @@ renderPetHealthIndex(calculatePetHealth(charmander.status))
 
 // 3. Pet Skill Upgrade
 export function upgradePetSkills (currentSkills, newSkills) {
+  return {
+    ...currentSkills,
+    ...newSkills
+  }
 }
 
 
@@ -34,13 +40,15 @@ renderSkillList(upgradePetSkills(pikachu.skills, newSkills))
 
 // 4. Rare Pet Protection
 export function protectRarePet (rarePet) {
+  return Object.freeze(rarePet)
 }
 
 
-renderPetProtectionStatus(protectRarePet(pikachu))
+renderPetProtectionStatus(protectRarePet(pikachu))  
 
 // 5. Pet Special Ability Check
 export function checkSpecialAbility (pet, abilityName) {
+  return pet.abilities.hasOwnProperty(abilityName)
 }
 
 
