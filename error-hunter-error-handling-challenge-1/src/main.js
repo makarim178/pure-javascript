3affb8151b485122642dfa1f1662c628e68250c6
Mir Ashiful Karim <MirAshiful.Karim@triosstudent.com>
2025-06-20 20:07:28 -0600

    Complete Error Hunter Challenge

--- error-hunter-error-handling-challenge-1/src/main.js
+++ error-hunter-error-handling-challenge-1/src/main.js
@@ -2,18 +2,28 @@
 
 // 1. Syntax Trap
 export function parseMagicSpell (spellString) {
-  const spellObject = JSON.parse(spellString)
-  return spellObject
+  try {
+    const spellObject = JSON.parse(spellString)    
+    return spellObject
+  } catch (error) {
+    return error
+  }
 }
 
 console.log(parseMagicSpell('{"spell": "fireball", "power": 10,}'))
+console.log(parseMagicSpell('{"spell": "fireball", "power": 10}'))
 
 // 2. Range Maze
 export function createMagicArray (length) {
-  return new Array(length)
+  try {
+    return new Array(length)    
+  } catch (error) {
+    return error
+  }
 }
 
 console.log(createMagicArray(-1))
+console.log(createMagicArray(1))
 
 // 3. Reference Enigma
 export function castSpell (spellName, target) {
@@ -22,9 +32,13 @@ export function castSpell (spellName, target) {
     'heal': (t) => `Heal ${t}`,
     'shield': (t) => `Apply shield to ${t}`
   }
-
-  return spells[spellName](target)
+  try {
+    return spells[spellName](target)    
+  } catch (error) {
+    return error
+  }
 }
 
 
 console.log(castSpell('invisibility', 'wizard'))
+console.log(castSpell('heal', 'wizard'))
